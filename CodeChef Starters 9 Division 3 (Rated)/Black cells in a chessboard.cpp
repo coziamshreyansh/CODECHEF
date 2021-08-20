9781188254bcf4aa8c664937f0b1c41835de36bc
Shreyansh Tiwari <60467135+coziamshreyansh@users.noreply.github.com>
2021-08-20 18:49:01 +0530

    Create  Black cells in a chessboard.cpp

---
+++ CodeChef Starters 9 Division 3 (Rated)/Black cells in a chessboard.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define lb lower_bound 
+#define nl "\n"
+#define fast() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+using namespace std;
+int main(){
+	fast();
+	int n; cin>>n;
+	cout<<(n*(n/2))<<nl;
+	return 0;
+}
